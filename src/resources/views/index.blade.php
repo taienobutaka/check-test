55f8e35ba58a39879fee157e6b44036a974345fc
taie <taie0424engineer@gmail.com>
2024-09-04 15:39:21 +0900

    更新

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Contact Form</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}" />
+</head>
+
+<body>
+    <header class="header">
+        <div class="header__inner">
+            <a class="header__logo">
+                FashionablyLate
+            </a>
+            <button class="form_login" type="submit">login</button>
+        </div>
+    <main>
+        <div class="contact-form__content">
+            <div class="contact-form__heading">
+                <h2>Register</h2>
+            </div>
+            <form class="form" action="/contacts/confirm" method="post">
+                @csrf
+                <div class="form__group">
+                    <div class="form__group-title">
+                        <span class="form__label--item">お名前</span>
+                    </div>
+                    <div class="form__group-content">
+                        <div class="form__input--text">
+                            <input type="text" name="name" placeholder="例: 山田 太郎" value="{{ old('name') }}" />
+                        </div>
+                        <div class="form__error">
+                            @error('name')
+                            {{ $message }}
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+                <div class="form__group">
+                    <div class="form__group-title">
+                        <span class="form__label--item">メールアドレス</span>
+                    </div>
+                    <div class="form__group-content">
+                        <div class="form__input--text">
+                            <input type="email" name="email" placeholder="例: test@example.com" value="{{ old('email') }}" />
+                        </div>
+                        <div class="form__error">
+                            @error('name')
+                            {{ $message }}
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+                <div class="form__group">
+                    <div class="form__group-title">
+                        <span class="form__label--item">パスワード</span>
+                    </div>
+                    <div class="form__group-content">
+                        <div class="form__input--text">
+                            <input type="tel" name="tel" placeholder="例: coachtech1106" value="{{ old('tel') }}" />
+                        </div>
+                        <div class="form__error">
+                            @error('name')
+                            {{ $message }}
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+                <div class="form__button">
+                    <button class="form__button-submit" type="submit">登録</button>
+                </div>
+            </form>
+        </div>
+    </main>
+</body>
+
+</html>
